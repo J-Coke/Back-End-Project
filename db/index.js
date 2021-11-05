4c636f4c55eb59129fd0d5d7417537ffab08d065
James Coker <jamescoker@Jamess-MacBook-Pro.local>
2021-11-05 14:54:28 +0000

    adds changes for heroku

--- db/index.js
+++ db/index.js
@@ -3,10 +3,21 @@ const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
+  
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+const config =
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);

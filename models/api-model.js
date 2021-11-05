2ffdd8039ef408f9ec56684212bd202867d5a5ea
James Coker <jamescoker@Jamess-MacBook-Pro.local>
2021-11-05 15:27:09 +0000

    adds part of get api

--- models/api-model.js
+++ models/api-model.js
@@ -1,13 +1,11 @@
 const db = require('../db');
+const { promises: fs } = require("fs")
+console.log('in api model')
 
 exports.fetchAllEndpoints = async () => {
+    const fileContents = await fs.readFile("NC-news-endpoints.json", "utf-8")
 
-    const {rows} = await db.query(`
-    SELECT *
-    FROM *
-    ;`)
-        
-    console.log(rows, 'ROWS')
+    console.log(fileContents)
 
-    return rows
+    return fileContents
 }
